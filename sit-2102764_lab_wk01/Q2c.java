55bb9053bac3e03ad1d92fff6217ebbd8efba51a
Kenny <40017759+Kenny-DevTech@users.noreply.github.com>
2022-01-26 12:16:01 +0800

    Add files via upload

---
+++ sit-2102764_lab_wk01/Q2c.java
@@ -0,0 +1,9 @@
+public class Q2c {
+    public static void main(String[] args) {
+        for(int x = 102; x >= 66; x--){
+            if(x%2==1){
+                System.out.println("value of x: " + x);
+            }
+        }
+    }
+}
